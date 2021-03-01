d9dcfdd83c444fab1ed8194d2d69001571f13bc4
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-03-01 17:59:35 -0300

    Updated package requirements in zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,7 @@
 # utils::globalVariables(c(".", "%>%", ":=", "%like%", "%chin%", "set"))
 
 .onLoad <- function(lib, pkg) {
-  requireNamespace("tidyverse")
   requireNamespace("sf")
-  requireNamespace("geodist")
 }
 
 # nocov end
